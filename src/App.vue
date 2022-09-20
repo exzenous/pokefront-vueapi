16f4dd7785077d9dd74986fb2b44f542fc4a99a2
Nathachai <exzenous@outlook.com>
2022-09-20 23:22:47 +0700

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+export default {
+  data() {
+    return {
+      value: 1
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello World {{ value }}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
