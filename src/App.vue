03ae052049e53e9680dc9a2efd0ded20148cc91a
Nathachai <exzenous@outlook.com>
2022-09-25 12:40:36 +0700

    PoC Axios/Display Mockup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <script lang="ts">
 
-export default {
-  data() {
-    return {
-      value: 1
-    }
+import { ref,defineComponent } from 'vue'
+import { getPokemon } from './components/pokeapi/fetch'
+
+export default defineComponent({
+  
+  props: {
+  },
+  created() {
+    const fetchedData = getPokemon(1)
+  },
+  setup() {
   }
-}
+
+})
 
 </script>
 
 <template>
   <div>
-    <h1>Hello World {{ value }}</h1>
+    <h1 class="text-center text-3xl py-4">PokeAPI | Front-end</h1>
   </div>
+
+  <div class="flex w-full flex-wrap">
+
+    <div v-for="n in 9" class="m-2 p-2 border-2 border-solid rounded grow text-center mywidth">
+      <h2 class="text-xl font-bold">Pokaemon #{{ n }}</h2>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png">
+      <p>PokeMon Name</p>
+      <p>Type: XXXXX</p>
+    </div>
+
+
+  </div>
+
 </template>
-
-<style>
-
-</style>
